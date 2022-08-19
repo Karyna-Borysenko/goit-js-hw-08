a1a548671b4df7d441737e654c1f7564f355fd4a
Karyna-Borysenko <k.a.borysenko@gmail.com>
2022-08-19 11:52:21 +0300

    01-gallery-done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+
+const galleryContainer = document.querySelector('.gallery');
+const imagesMarkup = createGallery(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li><a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a></li>`;
+    })
+    .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
