897b7e5e051957fbf8ecdca69d11784f399f9283
Karyna-Borysenko <k.a.borysenko@gmail.com>
2022-08-19 17:26:22 +0300

    02-video-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(timeUpdate, 1000));
+
+function timeUpdate({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+const localStorageCurrentTime = localStorage.getItem(
+  'videoplayer-current-time'
+);
+
+player
+  .setCurrentTime(localStorageCurrentTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
