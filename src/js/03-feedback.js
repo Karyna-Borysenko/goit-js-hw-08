6053fca401208c8547bd007e688ea574f1e3fbfa
Karyna-Borysenko <k.a.borysenko@gmail.com>
2022-08-20 10:53:38 +0300

    03-feedback-done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const formData = {};
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onInput, 500));
+
+populateInput();
+
+// при отправке убираем данные из хранилища, oчищаем форму
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (form.email.value === '' || form.message.value === '') {
+    return alert('Все поля должны быть заполнены!');
+  }
+  console.log({
+    email: form.email.value,
+    message: form.message.value,
+  });
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+// получаем значения полей и сохраняем их в хранилище
+
+function onInput(event) {
+  formData[event.target.name] = event.target.value;
+  const formDataJSON = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, formDataJSON);
+}
+
+// получаем значения из хранилища, если там что-то было, обновляем DOM
+
+function populateInput() {
+  const savedInput = localStorage.getItem(STORAGE_KEY);
+  const parsedSavedInput = JSON.parse(savedInput);
+  if (savedInput) {
+    form.email.value = parsedSavedInput.email;
+    form.message.value = parsedSavedInput.message;
+  }
+}
